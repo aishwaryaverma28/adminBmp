d1fe8e386227e8a84f217f4807281259e1b4330c
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-02-13 16:46:24 +0530

    build

--- src/components/bookmyplayer/BmpSidebar.jsx
+++ src/components/bookmyplayer/BmpSidebar.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/LPSetting.css";
 import "../styles/bmp.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import LeftArrow from "../../assets/image/arrow-left.svg";
-
+import Whatsapp from "../../assets/image/whatsapp.svg";
+import axios from "axios";
+import {
+  GET_ACADEMY
+} from "../utils/Constants";
 const BmpSidebar = () => {
   const userRole = localStorage.getItem("role_name");
   const navigate = useNavigate();
   const id = localStorage.getItem("academy_id");
+  const newId = localStorage.getItem("id");
+const [details, setDetails]= useState(null);
   const handleBackToTable = () => {
     localStorage.removeItem("academy_id");
     navigate("/bmp/admin");
   };
+
+  const academyDetails = () => {
+    axios
+      .post(GET_ACADEMY, { academy_id: newId }
+      //   , {
+      //   headers: {
+      //     Authorization: `Bearer ${decryptedToken}`,
+      //   },
+      // }
+      )
+      .then((response) => {
+        console.log(response?.data?.data);
+        setDetails(response?.data?.data[0])
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    academyDetails();
+  }, []);
+
+const sendWhatsAppMessage = () => {
+  const message = `I need more details for ${userRole} ${details.name} ${details.url}`;
+  const phoneNumber = "+447979100801";
+  // const phoneNumber = "+917558269998";
+  const encodedMessage = encodeURIComponent(message);
+  window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`, "_blank");
+};
 
   return (
     <section className="setting-side-panel">
@@ -79,6 +115,9 @@ const BmpSidebar = () => {
           Contact Support
         </NavLink>
       </p>
+      <div className="whatsapp_btn" onClick={sendWhatsAppMessage}>
+     <img src={Whatsapp} loadaing="lazy" alt="loading" width="14" height="14"></img>
+    </div>
     </section>
   );
 };
